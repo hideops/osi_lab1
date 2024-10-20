7a6886dad3dc2036454243bbe26a0d572b1bfd02
hideops <chevoda2908@gmail.com>
2024-10-20 12:00:00 -0700

    лаба1

--- child.c
+++ child.c
@@ -2,85 +2,95 @@
 #include <stdlib.h>
 
 int main() {
-    char buffer[256];
+    char buff[256];
     ssize_t bytesRead;
-    float totalSum;
-    char *currentChar;
+    float sm;
+    char *curr;
 
-    while ((bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer) - 1)) > 0) {
-        buffer[bytesRead] = '\0';
-        currentChar = buffer;
-        totalSum = 0.0f;
+    while ((bytesRead = read(STDIN_FILENO, buff, sizeof(buff) - 1)) > 0) {
+        buff[bytesRead] = '\0';
+        curr = buff;
+        sm = 0.0f;
 
-        while (*currentChar) {
-            while (*currentChar == ' ' || *currentChar == '\t' || *currentChar == '\n')
-                currentChar++;
+        while (*curr) {
+            while (*curr == ' ' || *curr == '\t' || *curr == '\n')
+                curr++;
 
             int sign = 1;
-            if (*currentChar == '-') {
+            if (*curr == '-') {
                 sign = -1;
-                currentChar++;
+                curr++;
             }
 
             float number = 0.0f;
             float fractional = 0.1f;
-            int hasDigits = 0;
+            int has_dig = 0;
 
-            while (*currentChar >= '0' && *currentChar <= '9') {
-                number = number * 10 + (*currentChar - '0');
-                currentChar++;
-                hasDigits = 1;
+            while (*curr >= '0' && *curr <= '9') {
+                number = number * 10 + (*curr - '0');
+                curr++;
+                has_dig = 1;
             }
 
-            if (*currentChar == '.') {
-                currentChar++;
-                while (*currentChar >= '0' && *currentChar <= '9') {
-                    number += (*currentChar - '0') * fractional;
+            if (*curr == '.') {
+                curr++;
+                while (*curr >= '0' && *curr <= '9') {
+                    number += (*curr - '0') * fractional;
                     fractional *= 0.1f;
-                    currentChar++;
-                    hasDigits = 1;
+                    curr++;
+                    has_dig = 1;
                 }
             }
 
-            if (hasDigits)
-                totalSum += number * sign;
+            if (has_dig)
+                sm += number * sign;
 
-            while (*currentChar && *currentChar != ' ' && *currentChar != '\t' && *currentChar != '\n')
-                currentChar++;
+            while (*curr && *curr != ' ' && *curr != '\t' && *curr != '\n')
+                curr++;
         }
 
         char output[64];
-        int outIndex = 0;
-        int integerPart = (int)totalSum;
-        float fractionPart = totalSum - integerPart;
+        int out_i = 0;
+        int integerPart = (int)sm;
+        float fractionPart = sm - integerPart;
         if (fractionPart < 0) fractionPart = -fractionPart;
 
         char tmpDigits[32];
-        int tmpIndex = 0;
-        int absInteger = integerPart < 0 ? -integerPart : integerPart;
-
-        do {
-            tmpDigits[tmpIndex++] = '0' + (absInteger % 10);
-            absInteger /= 10;
-        } while (absInteger > 0);
-
+        int tmp_i = 0;
+        int abs_num;
         if (integerPart < 0)
-            output[outIndex++] = '-';
+            abs_num = -integerPart;
+        else
+            abs_num = integerPart;
 
-        while (tmpIndex--)
-            output[outIndex++] = tmpDigits[tmpIndex];
-
-        output[outIndex++] = '.';
-
-        for (int i = 0; i < 2; i++) {
-            fractionPart *= 10;
-            int digit = (int)fractionPart;
-            output[outIndex++] = '0' + digit;
-            fractionPart -= digit;
+        if (abs_num == 0) {
+            tmpDigits[tmp_i++] = '0';
+        } else {
+            while (abs_num > 0) {
+                tmpDigits[tmp_i++] = '0' + (abs_num % 10);
+                abs_num = abs_num / 10;
+            }
         }
 
-        output[outIndex++] = '\n';
-        write(STDOUT_FILENO, output, outIndex);
+        if (integerPart < 0)
+            output[out_i++] = '-';
+
+        while (tmp_i > 0) {
+            output[out_i++] = tmpDigits[tmp_i - 1];
+            tmp_i--;
+        }
+
+        output[out_i++] = '.';
+
+        for (int i = 0; i < 2; i++) {
+            fractionPart = fractionPart * 10;
+            int digit = (int)fractionPart;
+            output[out_i++] = '0' + digit;
+            fractionPart = fractionPart - digit;
+        }
+
+        output[out_i++] = '\n';
+        write(STDOUT_FILENO, output, out_i);
     }
 
     return 0;
